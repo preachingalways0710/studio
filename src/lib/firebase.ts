41ee544fdd63aa262fce9286b951e50640ba1c3e
preachingalways <preachingalways@gmail.com>
2025-09-10 19:04:02 +0000

    ok, i imported the students and reloaded the screen here and they did no

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,6 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   "projectId": "studio-6494355702-581e6",
+  "databaseURL": "https://studio-6494355702-581e6-default-rtdb.southamerica-east1.firebasedatabase.app",
   "appId": "1:403606666583:web:d1f3c8d9e28f7e7421d212",
   "storageBucket": "studio-6494355702-581e6.firebasestorage.app",
   "apiKey": "AIzaSyD_9qflTHgthrxHqONQdRnkMxpHE9UvhiE",
