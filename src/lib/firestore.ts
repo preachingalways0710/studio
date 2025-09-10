9d97dbed0e8edcc77ad610bfd000c3795c1b6f54
preachingalways <preachingalways@gmail.com>
2025-09-10 19:15:55 +0000

    nope how can i download htis project and begin working on it somewhere e

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -1,21 +1,23 @@
 'use client';
 
 import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, experimentalForceLongPolling } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "studio-6494355702-581e6",
-  "appId": "1:403606666583:web:d1f3c8d9e28f7e7421d212",
-  "storageBucket": "studio-6494355702-581e6.firebasestorage.app",
-  "apiKey": "AIzaSyD_9qflTHgthrxHqONQdRnkMxpHE9UvhiE",
-  "authDomain": "studio-6494355702-581e6.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "403606666583"
+  projectId: 'studio-6494355702-581e6',
+  appId: '1:403606666583:web:d1f3c8d9e28f7e7421d212',
+  storageBucket: 'studio-6494355702-581e6.firebasestorage.app',
+  apiKey: 'AIzaSyD_9qflTHgthrxHqONQdRnkMxpHE9UvhiE',
+  authDomain: 'studio-6494355702-581e6.firebaseapp.com',
+  messagingSenderId: '403606666583',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
 const auth = getAuth(app);
+const db = getFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+
 
 export { db, auth };
